d9e84b910d3b4d0606133d043cb7d7e9e7ee8245
Harshit Chouhan <harshitsingh.chouhan@techgrit.com>
2024-08-04 00:00:24 +0530

    "Updated Angular project configuration, added PrimeIcons and PrimeNG dependencies, and modified login component HTML, SCSS, and TS files."

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
+import { ButtonModule } from 'primeng/button';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule 
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  value!: string;
+
   constructor(private titleService: Title) {}
 
   ngOnInit(): void {
