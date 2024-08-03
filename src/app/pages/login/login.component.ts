034bf0ea4b8cd3145b249e05e52c6a6a73173915
Harshit Chouhan <harshitsingh.chouhan@techgrit.com>
2024-08-03 22:14:11 +0530

    "Updated login component HTML and added title setting in TypeScript file"

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  constructor(private titleService: Title) {}
 
+  ngOnInit(): void {
+    this.titleService.setTitle('Login');
+  }
 }
