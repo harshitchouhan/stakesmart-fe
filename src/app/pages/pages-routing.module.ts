f0a6e1b07ff02ec0918b6b4c6a9f8a248138048e
Harshit Chouhan <harshitsingh.chouhan@techgrit.com>
2024-08-03 22:06:32 +0530

    "Updated app component HTML and TS files, and added routes configuration"

---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      {
+        path: 'login',
+        loadChildren: () =>
+          import('./login/login.module').then((m) => m.LoginModule),
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PagesRoutingModule {}
---
+++ src/app/pages/pages.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-pages',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  templateUrl: './pages.component.html',
+  styleUrl: './pages.component.scss',
+})
+export class PagesComponent {}
