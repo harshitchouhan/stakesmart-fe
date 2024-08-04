fb122208b581d851bec7031c5fee402ea2ce4392
Harshit Chouhan <harshitsingh.chouhan@techgrit.com>
2024-08-04 10:25:18 +0530

    "Updated OTP component HTML and TS files: added submit button and imported ButtonModule"

--- src/app/shared/otp/otp.component.ts
+++ src/app/shared/otp/otp.component.ts
@@ -1,11 +1,12 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 import { InputOtpModule } from 'primeng/inputotp';
 
 @Component({
   selector: 'app-otp',
   standalone: true,
-  imports: [InputOtpModule, FormsModule],
+  imports: [InputOtpModule, FormsModule, ButtonModule],
   templateUrl: './otp.component.html',
   styleUrl: './otp.component.scss',
   encapsulation: ViewEncapsulation.None,
