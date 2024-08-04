28ecade33acb609f06ca5f79b9ad464ecf5df1d7
Harshit Chouhan <harshitsingh.chouhan@techgrit.com>
2024-08-04 13:29:31 +0530

    "Updated Mines component HTML, SCSS, and TS files; added grid layout and styling for mines container and buttons"

--- src/app/games/mines/mines.component.ts
+++ src/app/games/mines/mines.component.ts
@@ -1,14 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { TabViewModule } from 'primeng/tabview';
 import { TooltipModule } from 'primeng/tooltip';
+
 import { ManualInputsComponent } from './manual-inputs/manual-inputs.component';
 
 @Component({
   selector: 'app-mines',
   standalone: true,
-  imports: [TabViewModule, TooltipModule, ManualInputsComponent],
+  imports: [
+    TabViewModule,
+    TooltipModule,
+    ManualInputsComponent,
+    ButtonModule,
+    CommonModule,
+  ],
   templateUrl: './mines.component.html',
   styleUrl: './mines.component.scss',
   encapsulation: ViewEncapsulation.None,
 })
-export class MinesComponent {}
+export class MinesComponent {
+  mines: any = [];
+
+  constructor() {
+    this.mines = [];
+    for (let i = 1; i <= 25; i++) {
+      this.mines.push({ label: i, value: i });
+    }
+  }
+}
