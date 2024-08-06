615b6ec9283c64937a75751aecb287ccdbfe0130
Harshit Chouhan <harshitsingh.chouhan@techgrit.com>
2024-08-06 15:57:40 +0530

    "Updated Mines component HTML, SCSS, and TS files to add new functionality and styling for mine selection and bomb effects."

--- src/app/games/mines/mines.component.ts
+++ src/app/games/mines/mines.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TabViewModule } from 'primeng/tabview';
 import { TooltipModule } from 'primeng/tooltip';
@@ -21,12 +21,45 @@ import { ManualInputsComponent } from './manual-inputs/manual-inputs.component';
   encapsulation: ViewEncapsulation.None,
 })
 export class MinesComponent {
+  @ViewChild('beepAudio') beepAudio!: ElementRef;
+  @ViewChild('bombAudio') bombAudio!: ElementRef;
+  
   mines: any = [];
+
+  selectedMines: any = [];
+  selectedMinesIds: any = [];
+  selectedMine: any;
+  disableGame = false;
 
   constructor() {
     this.mines = [];
     for (let i = 1; i <= 25; i++) {
-      this.mines.push({ label: i, value: i });
+      this.mines.push({ revealed: false, bomb: false, id: i, selected: false });
+    }
+
+    this.mines[4].bomb = true;
+    this.mines[19].bomb = true;
+    this.mines[14].bomb = true;
+  }
+
+  onMineClick(mine: any) {
+    console.log(mine);
+    if (this.disableGame) {
+      return;
+    }
+
+    this.selectedMine = mine;
+    if (!mine.bomb) {
+      mine.selected = true;
+      this.selectedMines.push(mine);
+      this.selectedMinesIds.push(mine.id);
+      this.beepAudio.nativeElement.play();
+    } else {
+      this.bombAudio.nativeElement.play();
+      this.disableGame = true;
+      this.mines.forEach((value: any) => {
+        value.revealed = true;
+      });
     }
   }
 }
