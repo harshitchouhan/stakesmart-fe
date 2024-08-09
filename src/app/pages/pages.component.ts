9e3a2c3dbab7fcfed6df5e019f10815a7635837a
Harshit Chouhan <harshitsingh.chouhan@techgrit.com>
2024-08-09 22:54:34 +0530

    "Moved topbar component from home component to pages component, updated styles and templates accordingly."

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TopbarComponent } from '../shared/topbar/topbar.component';
 
 @Component({
   selector: 'app-pages',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, TopbarComponent],
   templateUrl: './pages.component.html',
   styleUrl: './pages.component.scss'
 })
--- src/app/shared/topbar/topbar.component.ts
+++ src/app/shared/topbar/topbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BadgeModule } from 'primeng/badge';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [BadgeModule],
+  imports: [BadgeModule, RouterModule],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss',
 })
